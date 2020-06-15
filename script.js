817b4f24773b90e995dee2f25e5789d8ff3cd9ff
Sean Hughes <seanjhughes92@gmail.com>
2020-06-15 12:10:30 +0100

    added mobile nav

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+const burger = document.getElementById("burger");
+const mobileNav = document.querySelector("nav")
+
+
+burger.addEventListener('click', openMenu => {
+    if (mobileNav.style.display === 'none') {
+        mobileNav.style.display = 'flex';
+    } else {
+        mobileNav.style.display = 'none';
+    }
+});
